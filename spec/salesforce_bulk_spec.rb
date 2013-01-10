8b1240550667d6714d1272fbc602606374c18cc6
Arne Brasseur <arne.brasseur@rendaosolutions.com>
2013-01-10 17:50:01 +0100

    Fix the spec

--- spec/salesforce_bulk_spec.rb
+++ spec/salesforce_bulk_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe SalesforceBulk do
   it "should login" do
-    SalesforceBulk::Api.new( username, password, login_host ).connection.login
+    SalesforceBulk::Api.new( username, password, login_host )
 
     service.should_have_received_login
   end
